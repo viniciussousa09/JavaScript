bcc5ee44f67678cb7d5805dbf86de16efd2411ad
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-05-05 23:19:59 -0300

    JS Estruturas

---
+++ 3_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,17 @@
+// 1 - Variáveis
+let nome = "Vinicius";
+
+console.log(nome);
+
+nome = "Vinicius Sousa";
+
+console.log(nome);
+
+const idade = 38;
+
+console.log(idade);
+
+// idade = 38;
+
+console.log(typeof nome);
+console.log(typeof idade);
