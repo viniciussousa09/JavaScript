0f37d6dd5ee42cb6205c6801b986c3e126ee71a7
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-05-10 18:09:29 -0300

    Funções JS

--- 4_FUNCOES/js/scripts.js
+++ 4_FUNCOES/js/scripts.js
@@ -179,3 +179,57 @@ function someFunction() {
 
 someFunction();
 
+// 10 - mais sobre closure
+const multiplicationClosure = (n) => {
+    return (m) => {
+        return n * m;
+    };
+};
+
+const c1 = multiplicationClosure(5);
+
+const c2 = multiplicationClosure(10);
+
+console.log(c1);
+
+console.log(c2);
+
+console.log(c1(5));
+
+console.log(c2(10));
+
+// 11 - recursion
+const untilTen = (n, m) => {
+    if (n < 10) {
+        console.log("A fun;ão parou de executar!");
+    } else {
+        const x = n - m;
+
+        console.log(x);
+
+        untilTen(x, m);
+    }
+};
+
+untilTen(100, 7);
+
+// infinite recursion
+// function run() {
+//     console.log("Executando...");
+//     run();
+// }
+// run();
+
+function factorial(x) {
+    if (x === 0) {
+        return 1;
+    } else {
+        return x * factorial(x - 1);
+    }
+}
+
+const num = 6;
+
+const result = factorial(num);
+
+console.log(`O fatorial do número ${num} é ${result}`);
