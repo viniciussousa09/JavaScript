c555e08fda035abd8b6085042c51506191ea7aa1
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-05-26 23:47:46 -0300

    Debug e tratamento de erros

--- 7_DEBUG/js/scripts.js
+++ 7_DEBUG/js/scripts.js
@@ -72,8 +72,42 @@ checkNumber({});
 checkNumber("teste");
 
 // 5 - Exceptions
-let x = 10
+let x = 10;
 
-if(x != 11) {
-    throw new Error("O valor de x não pode ser diferente de 11")
+if (x != 11) {
+    // throw new Error("O valor de x não pode ser diferente de 11");
 }
+
+// 6 - try catch
+try {
+    const soma = x + y;
+} catch (error) {
+    console.log(`Erro no programa: ${error}`);
+}
+
+// 7 - Finally
+try {
+    const value = checkNumber("1");
+
+    if (!value) {
+        throw new Error("Valores inválidos");
+    }
+} catch (error) {
+    console.log(`Opa, aconteceu um problema: ${error}`);
+} finally {
+    console.log("O código foi executado");
+}
+
+// 8 - assertions
+function checkArray(arr) {
+
+    if (arr.length === 0) {
+        throw new Error("O array precisa ter elementos");
+    } else {
+        console.log(`O array tem ${arr.length} elementos`);
+    }
+}
+
+// checkArray([]);
+
+checkArray([1, 2 ,3])
