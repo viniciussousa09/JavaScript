6a8486e0b54490a8a1256e15cf9d9250e71f27fa
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-05-27 21:40:00 -0300

    Async

---
+++ 8_ASINC/js/scripts.js
@@ -0,0 +1,62 @@
+// //1 - setTimeout
+// console.log("Ainda não executou");
+
+// setTimeout(function () {
+//     console.log("Requisição assincrona");
+// }, 2000);
+
+// console.log("Ainda não executou 2");
+
+// //2 - setInterval
+// console.log("Ainda não começou");
+
+// // setInterval(function () {
+// //     console.log("Intervalo assíncrono");
+// // }, 3000);
+
+// console.log("Ainda não começou 2");
+
+// //3 - promise
+// const promessa = Promise.resolve(5 + 5);
+
+// console.log("Algum código");
+
+// promessa.then((value) => {
+//     console.log(`A soma é ${value}`);
+//     return value;
+// })
+//     .then((value) => value - 1)
+//     .then((value) => console.log(`Agora é ${value}`));
+
+// console.log("Outro código");
+
+// // 4 - falha na promise
+// Promise.resolve(4 * "asd")
+//     .then((n) => {
+//         if (Number.isNaN(n)) {
+//             throw new Error("Valores inválidos");
+//         }
+//     })
+//     .catch((err) => console.log(`Um erro ocorreu: ${err}`));
+
+// 5 - rejeição
+function checkNumber(n) {
+    return new Promise((resolve, reject) => {
+        if (n > 10) {
+            resolve(`O número é maior que 10`);
+        } else {
+            reject(new Error("Número muito baixo"));
+        }
+    });
+}
+
+const a = checkNumber(20);
+
+const b = checkNumber(10);
+
+
+a.then((v) => console.log(`o valor é ${v}`)).catch((err) => console.log(`Um erro ocorreu ${err}`)
+);
+
+b.then((v) => console.log(`o valor é ${v}`)).catch((err) => console.log(`Um erro ocorreu ${err}`)
+);
