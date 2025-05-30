4e9ba7ab99c6e38ac46ef57ff4cc58ccce4c3d6a
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-05-29 22:36:58 -0300

    Promises
    resolvendo varias promises

--- 8_ASINC/js/scripts.js
+++ 8_ASINC/js/scripts.js
@@ -55,8 +55,29 @@ const a = checkNumber(20);
 const b = checkNumber(10);
 
 
-a.then((v) => console.log(`o valor é ${v}`)).catch((err) => console.log(`Um erro ocorreu ${err}`)
+a.then((v) => console.log(`o resultado é ${v}`)).catch((err) => console.log(`Um erro ocorreu ${err}`)
 );
 
 b.then((v) => console.log(`o valor é ${v}`)).catch((err) => console.log(`Um erro ocorreu ${err}`)
 );
+
+// 6 - resolvendo varias promises
+const p1 = new Promise((resolve, reject) => {
+    setTimeout(function () {
+        resolve(10);
+    }, 3000);
+});
+
+const p2 = Promise.resolve(10 + 10);
+
+const p3 = new Promise((resolve, reject) => {
+    if (30 > 10) {
+        resolve(30);
+    } else {
+        reject("Erro!");
+    }
+});
+
+Promise.all([p1, p2, p3]).then((values) => console.log(values));
+
+// 7 - 
