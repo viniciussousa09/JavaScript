b19858abd4a2818ccc1321cca1a4fe3c18eb65c5
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-02 20:34:17 -0300

    atributos

--- 9_DOM/js/scripts.js
+++ 9_DOM/js/scripts.js
@@ -11,3 +11,78 @@ console.log(document.body.childNodes[1].childNodes[1].textContent);
 const listItens = document.getElementsByTagName("li");
 
 console.log(listItens);
+
+// 3 - selecionando elemento por id
+const title = document.getElementById("title");
+
+console.log(title);
+
+// 4 - selecionando elemento por classe
+const products = document.getElementsByClassName("product");
+
+console.log(products);
+
+// 5 - selecionando elemento por CSS
+const productsQuery = document.querySelectorAll(".product");
+
+console.log(productsQuery);
+
+const mainContainer = document.querySelector("#main-container");
+
+console.log(mainContainer);
+
+// 6 - insertbefore
+const p = document.createElement("p");
+
+const header = title.parentElement;
+
+header.insertBefore(p, title);
+
+// 7 - appendChild
+const navLinks = document.querySelector("nav ul");
+
+const li = document.createElement("li");
+
+navLinks.appendChild(li);
+
+// 8 - replaceChild
+const h2 = document.createElement("h2");
+
+h2.textContent = "Meu novo título";
+
+header.replaceChild(h2, title);
+
+// 9 - creatTextNode
+const myText = document.createTextNode("Agora vamos colocar mais um título");
+
+console.log(myText);
+
+const h3 = document.createElement("h3");
+
+h3.appendChild(myText);
+
+console.log(h3);
+
+mainContainer.appendChild(h3);
+
+// 10 - trabalhando com atributos
+const firstLink = navLinks.querySelector("a");
+
+console.log(firstLink);
+
+firstLink.setAttribute("href", "https://www.google.com");
+
+console.log(firstLink.getAttribute("href"));
+
+firstLink.setAttribute("target", "_blank");
+
+// 11 - altura e largura
+const footer = document.querySelector("footer");
+
+console.log(footer.offsetWidth);
+console.log(footer.offsetHeight);
+
+console.log(footer.clientWidth);
+console.log(footer.clientHeight);
+
+// 12 - posiç
