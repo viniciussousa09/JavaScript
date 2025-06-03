acb180e96bbdd16945095e044e84bc19942cd145
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-02 21:55:00 -0300

    Eventos

--- 10_EVENTOS/js/scripts.js
+++ 10_EVENTOS/js/scripts.js
@@ -30,3 +30,25 @@ myTitle.addEventListener("click", (event) => {
     console.log(event.pointerType);
     console.log(event.target);
 });
+
+// 4 - propagação
+const containerBtn = document.querySelector("#btn-container");
+const btnInsideContainer = document.querySelector("#div-btn");
+
+containerBtn.addEventListener("click", () => {
+    console.log("Evento 1");
+});
+
+containerBtn.addEventListener("click", (e) => {
+    e.stopPropagation();
+    console.log("Evento 2");
+});
+
+// 5 - Removendo evento padrão
+const a = document.querySelector("a");
+
+a.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    console.log("Não alterou a página");
+});
