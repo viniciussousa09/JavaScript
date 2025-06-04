511aeb12afe07c5e6021f41e6674a57e5f2eeed8
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-03 21:17:07 -0300

    Eventos

--- 10_EVENTOS/js/scripts.js
+++ 10_EVENTOS/js/scripts.js
@@ -52,3 +52,82 @@ a.addEventListener("click", (e) => {
 
     console.log("Não alterou a página");
 });
+
+// 6 - eventos de tecla
+document.addEventListener("keyup", (e) => {
+    console.log(`Soltou a tecla ${e.key}`);
+});
+
+document.addEventListener("keydown", (e) => {
+    console.log(`Apertou a tecla ${e.key}`);
+});
+
+// 7 - eventos de mouse
+const mouseEvents = document.querySelector("#mouse");
+
+mouseEvents.addEventListener("mousedown", () => {
+    console.log("Pressionou o botão");
+});
+
+mouseEvents.addEventListener("mouseup", () => {
+    console.log("Soltou o botão");
+});
+
+mouseEvents.addEventListener("dblclick", () => {
+    console.log("Clique duplo");
+});
+
+// 8 - movimento do mouse
+// document.addEventListener("mousemove", (e) => {
+//     console.log(`No eixo X: ${e.x}`);
+//     console.log(`No eixo Y: ${e.y}`);
+// });
+
+// 9 - evento de scroll
+window.addEventListener("scroll", (e) => {
+    if (window.pageYOffset > 200) {
+        console.log("Passamos de 200px");
+    }
+});
+
+// 10 - evento de focus/blur
+const input = document.querySelector("#my-input");
+
+input.addEventListener("focus", (e) => {
+    console.log("Entrou no input");
+});
+
+input.addEventListener("blur", (e) => {
+    console.log("Saiu no input");
+});
+
+// 11 - evento de carregamento 
+window.addEventListener("load", () => {
+    console.log("A página carregou!");
+});
+
+window.addEventListener("beforeunload", (e) => {
+    e.preventDefault();
+    e.returnValue = "teste";
+});
+
+// 12 - debounce
+const debounce = (f, delay) => {
+
+    let timeout;
+
+    return (...arguments) => {
+        if (timeout) {
+            clearTimeout(timeout);
+        }
+
+        timeout = setTimeout(() => {
+            f.apply(arguments);
+        }, delay);
+    };
+};
+
+window.addEventListener("mousemove", debounce(() => {
+    console.log("Executando a cada 400ms");
+}, 400)
+);
