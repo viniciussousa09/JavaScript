e0508de142b8340b8d120abf919b42e4c7181266
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-05 19:51:43 -0300

    tabuada

--- TABUADA/js/scripts.js
+++ TABUADA/js/scripts.js
@@ -1,1 +1,37 @@
-console.log("teste")
+//  Seleção de elementos
+const multiplicationForm = document.querySelector("#multiplication-form");
+const numberInput = document.querySelector("#number");
+const multiplicationInput = document.querySelector("#multiplicatior");
+
+const multiplicationTable = document.querySelector(
+    "#multiplicator-operations"
+);
+
+//  Funções
+const createTable = (number, multiplicatorNumber) => {
+
+    multiplicationTable.innerHTML = "";
+
+    for (i = 1; i < multiplicatorNumber; i++) {
+
+        const result = number * i;
+
+        console.log(result);
+
+    }
+
+};
+
+//  Eventos
+multiplicationForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const multiplicationNumber = +numberInput.value;
+
+    const multiplicatorNumber = +multiplicationInput.value;
+
+    if (!multiplicationNumber || !multiplicatorNumber) return;
+
+    createTable(multiplicationNumber, multiplicatorNumber);
+
+});
