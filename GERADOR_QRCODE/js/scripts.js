130007ac31022a7d25b9ca73134576af718536df
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-06 22:34:06 -0300

    qr code

--- GERADOR_QRCODE/js/scripts.js
+++ GERADOR_QRCODE/js/scripts.js
@@ -1,1 +1,23 @@
-Console.log("Avanti!")
+const container = document.querySelector(".container");
+const qrCodeBtn = document.querySelector("#qr-form button");
+const qrCodeInput = document.querySelector("#qr-form input");
+const qrCodeImg = document.querySelector("#qr-code img");
+
+// Eventos
+function generateQrCode() {
+    const qrCodeInputValue = qrCodeInput.value;
+
+    if (!qrCodeInputValue) return;
+
+    qrCodeBtn.innerText = "Gerando código...";
+
+    qrCodeImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${qrCodeInputValue}`;
+
+    qrCodeImg.addEventListener("load", () => {
+        container.classList.add("active");
+    })
+}
+
+qrCodeBtn.addEventListener("click", () => {
+    generateQrCode();
+});
