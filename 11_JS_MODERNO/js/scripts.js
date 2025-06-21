5147e2efc0fc595252dbaea53a765570c16e0650
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-06-20 21:18:19 -0300

    herança

--- 11_JS_MODERNO/js/scripts.js
+++ 11_JS_MODERNO/js/scripts.js
@@ -172,3 +172,30 @@ console.log(shirt);
 
 console.log(shirt.productWithDiscount(10));
 console.log(shirt.productWithDiscount(15));
+
+// 9 - herança
+class productWithAttributes extends Product {
+    constructor(name, price, colors) {
+        super(name, price);
+        this.colors = colors;
+    }
+
+    showColors() {
+        console.log("As cores são: ");
+        this.colors.forEach((color) => {
+            console.log(color);
+        });
+    }
+}
+
+const hat = new productWithAttributes("Chapéu", 29.99, [
+    "Preto",
+    "Amarelo",
+    "Verde",
+]);
+
+console.log(hat);
+
+console.log(hat.name);
+
+hat.showColors();
