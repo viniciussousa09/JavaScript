69409224c6cdde27a8118bbefc2a5fc5dcc064d2
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-08-04 21:32:05 -0300

    Iniciando função de geração de caracteres

--- GERADOR_SENHA/js/scripts.js
+++ GERADOR_SENHA/js/scripts.js
@@ -1,1 +1,28 @@
-console.log("teste");
+// Seleção de Elementos
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordButton = document.querySelector("#generated-password");
+
+// Funções
+const getLetterLowercase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+};
+
+const getLetterUppercase = () => {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+
+const getNumber = () => {
+    return Math.floor(Math.random() * 10).toString();
+};
+
+const getSymbol = () => {
+    const symbols = "(){}[]=></.,!@#$%&*+-";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+};
+
+console.log(getSymbol());
+
+// Eventos
+generatePasswordButton.addEventListener("click", () => {
+    console.log("teste");
+});
