0a1357880ee4dc3da90f88935bc970f724bada1c
Vinicius Oliveira Sousa <vinicius.oliveirasousa09@gmail.com>
2025-08-18 21:25:26 -0300

    Excluindo e duplicando notas

--- DEV_NOTES/js/scripts.js
+++ DEV_NOTES/js/scripts.js
@@ -64,6 +64,18 @@ function creatNote(id, content, fixed) {
 
     element.appendChild(pinIcon);
 
+    const deleteIcon = document.createElement("i");
+
+    deleteIcon.classList.add(...["bi", "bi-x-lg"]);
+
+    element.appendChild(deleteIcon);
+
+    const duplicateIcon = document.createElement("i");
+
+    duplicateIcon.classList.add(...["bi", "bi-file-earmark-plus"]);
+
+    element.appendChild(duplicateIcon);
+
     if (fixed) {
         element.classList.add("fixed");
     }
@@ -71,6 +83,14 @@ function creatNote(id, content, fixed) {
     // Eventos do elemento
     element.querySelector(".bi-pin").addEventListener("click", () => {
         toggleFixNote(id);
+    });
+
+    element.querySelector(".bi-x-lg").addEventListener("click", () => {
+        deleteNote(id, element);
+    });
+
+    element.querySelector(".bi-file-earmark-plus").addEventListener("click", () => {
+        copyNote(id);
     });
 
     return element;
@@ -86,6 +106,35 @@ function toggleFixNote(id) {
     saveNotes(notes);
 
     showNotes();
+}
+
+function deleteNote(id, element) {
+    const notes = getNotes().filter((note) => note.id !== id);
+
+    saveNotes(notes);
+
+    notesContainer.removeChild(element);
+}
+
+function copyNote(id) {
+
+    const notes = getNotes();
+
+    const targetNote = notes.filter((note) => note.id === id)[0];
+
+    const noteObject = {
+        id: generateId(),
+        content: targetNote.content,
+        fixed: false,
+    };
+
+    const noteElement = creatNote(noteObject.id, noteObject.content, noteObject.fixed);
+
+    notesContainer.appendChild(noteElement);
+
+    notes.push(noteObject);
+
+    saveNotes(notes);
 }
 
 
